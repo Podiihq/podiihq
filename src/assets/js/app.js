41dc023f8fa1c7071588d7a288bab64680638886
musaomondi <amokem@yahoo.com>
2018-03-23 10:59:23 +0300

    add jQuery counter

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -10,3 +10,14 @@ import Foundation from 'foundation-sites';
 
 
 $(document).foundation();
+$('.counter').each(function () {
+  $(this).prop('Counter',0).animate({
+    Counter: $(this).text()
+  }, {
+    duration: 5000,
+    easing: 'swing',
+    step: function (now) {
+      $(this).text(Math.ceil(now));
+    }
+  });
+});
