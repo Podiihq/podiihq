b958bc5b47531746d8169f897644c6968de8b6ea
musaomondi <amokem@yahoo.com>
2018-03-29 09:24:29 +0300

    remove counter function

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -8,16 +8,3 @@ import Foundation from 'foundation-sites';
 // the line below
 //import './lib/foundation-explicit-pieces';
 
-
-$(document).foundation();
-$('.counter').each(function () {
-  $(this).prop('Counter',0).animate({
-    Counter: $(this).text()
-  }, {
-    duration: 5000,
-    easing: 'swing',
-    step: function (now) {
-      $(this).text(Math.ceil(now));
-    }
-  });
-});
