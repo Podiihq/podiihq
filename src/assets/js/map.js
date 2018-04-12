fcacf98169418940c1f4ea135e647d0bd0894cae
Lmise <lucymuhonja@gmail.com>
2018-04-12 10:32:08 +0300

    Add map location of podii office

---
+++ src/assets/js/map.js
@@ -0,0 +1,98 @@
+google.maps.event.addDomListener(window, 'load', init);
+
+function init() {
+  var mapOptions = {
+    zoom: 17,
+    scrollwheel: false,
+
+    center: new google.maps.LatLng(-0.1076263, 34.76850450000006),
+    styles: [
+      {
+        featureType: 'administrative',
+        elementType: 'labels.text.fill',
+        stylers: [
+          {
+            color: '#000000'
+          }
+        ]
+      },
+      {
+        featureType: 'landscape',
+        elementType: 'all',
+        stylers: [
+          {
+            color: '#f2f2f2'
+          }
+        ]
+      },
+      {
+        featureType: 'poi',
+        elementType: 'all',
+        stylers: [
+          {
+            visibility: 'off'
+          }
+        ]
+      },
+      {
+        featureType: 'road',
+        elementType: 'all',
+        stylers: [
+          {
+            saturation: -100
+          },
+          {
+            lightness: 45
+          }
+        ]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'all',
+        stylers: [
+          {
+            visibility: 'simplified'
+          }
+        ]
+      },
+      {
+        featureType: 'road.arterial',
+        elementType: 'labels.icon',
+        stylers: [
+          {
+            visibility: 'off'
+          }
+        ]
+      },
+      {
+        featureType: 'transit',
+        elementType: 'all',
+        stylers: [
+          {
+            visibility: 'off'
+          }
+        ]
+      },
+      {
+        featureType: 'water',
+        elementType: 'all',
+        stylers: [
+          {
+            color: '#BBBBBB'
+          },
+          {
+            visibility: 'on'
+          }
+        ]
+      }
+    ]
+  };
+
+  var mapElement = document.getElementById('map');
+  var map = new google.maps.Map(mapElement, mapOptions);
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(-0.1076263, 34.76850450000006),
+    map: map,
+    title: 'Here is our offices'
+  });
+}
