348114b594f6d4bfad734e942a32cc6228883953
sigu <sigumagwa@gmail.com>
2018-04-24 16:28:04 +0300

    fix css and missing html tags

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,14 +3,14 @@ import whatInput from 'what-input';
 
 window.$ = $;
 
-import Foundation from 'foundation-sites';
+// import Foundation from 'foundation-sites';
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
-//import './lib/foundation-explicit-pieces';
+import './lib/foundation-explicit-pieces';
+
+import './map.js';
 
 imagesLoaded(document.querySelector('body'), function(instance) {
   document.getElementById('loader').style.display = 'none';
   document.getElementById('page-content').style.display = 'block';
 });
-
-import './map.js';
