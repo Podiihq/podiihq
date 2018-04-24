3a4045e4ee72d02ea5485e3b2c73a65de572665b
sigu <sigumagwa@gmail.com>
2018-04-24 21:42:58 +0300

    Fix: speed suggestions

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -14,3 +14,15 @@ imagesLoaded(document.querySelector('body'), function(instance) {
   document.getElementById('loader').style.display = 'none';
   document.getElementById('page-content').style.display = 'block';
 });
+
+var Tawk_API = Tawk_API || {},
+  Tawk_LoadStart = new Date();
+(function() {
+  var s1 = document.createElement('script'),
+    s0 = document.getElementsByTagName('script')[0];
+  s1.async = true;
+  s1.src = 'https://embed.tawk.to/5ac4b573d7591465c7092dfd/default';
+  s1.charset = 'UTF-8';
+  s1.setAttribute('crossorigin', '*');
+  s0.parentNode.insertBefore(s1, s0);
+})();
