0e288d90ec06ea01dad5b016ac4543f656038773
sigu <sigumagwa@gmail.com>
2018-04-25 09:30:24 +0300

    Fix: defer running of map script

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,14 +1,14 @@
+import initialiseMap from './map';
 import $ from 'jquery';
 import whatInput from 'what-input';
 
 window.$ = $;
+window.initialiseMap = initialiseMap;
 
 import Foundation from 'foundation-sites';
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
 // import './lib/foundation-explicit-pieces';
-
-import './map.js';
 
 imagesLoaded(document.querySelector('body'), function(instance) {
   document.getElementById('loader').style.display = 'none';
